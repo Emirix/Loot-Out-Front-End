9945bc56e5e0526a48f85d1f06d6506e027b0f16
Emir Tanır <emirtanir55@gmail.com>
2021-05-19 16:53:51 +0300

    19 mayıs

--- src/views/components/OnSaleItem.jsx
+++ src/views/components/OnSaleItem.jsx
@@ -8,11 +8,17 @@ export default function OnSaleItem() {
             <div className="about ps-2">
                 <div className="urun-adi">Air jordan m1</div>
                 <div className="added-date">added on 28 March</div>
+                <div className="mobile-fer-fiyat">2000 €</div>
             </div>
 
-            <div className="fiyat">2 000 £</div>
-            <div className="edit">Edit</div>
-            <div className="remove">Remove</div>
+            <div className="fiyat ">2 000 €</div>
+            <div className="edit otuzbir">Edit</div>
+            <div className="remove otuzbir">Remove</div>
+            <div className="mobile-fer">
+            <div className="otuzbir">Edit</div>
+            <div className="otuzbir">Remove</div>
+          
+            </div>
         </div>
     )
 }
