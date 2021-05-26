1dcb9bbcbbf3c7d2ab974d7d2c360063981f3a54
Emir Tanır <emirtanir55@gmail.com>
2021-05-26 14:26:53 +0300

    blo

--- src/views/pages/Blog.jsx
+++ src/views/pages/Blog.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../../shared/Header'
-import BG from "../../assets/img/blog-bg1366.jpg"
+import BG from "../../assets/img/blog-bg1440.jpg"
+import Atom from "../../assets/img/blog-atom.png"
 
 function Blog() {
     return (
         <div className="blog-page-container container-fluid">
-            <Header/>
+            <Header/>  
             <img className="blog-bg" src={BG}/>
-        </div>
+        </div> 
     )
 }
 
